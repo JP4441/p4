2181403dbd9efbe207af8f201029c61e0a29ff4a
My Mac <home@Js-MacBook-Pro.local>
2022-04-24 15:22:56 -0400

    P4 update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <MainNavigation />
+      <main>
+    <Switch>
     <Route path="/" exact>
       <Users />
     </Route>
@@ -15,6 +18,7 @@ const App = () => {
     </Route>
         <Redirect to="/" />
         </Switch>
+        </main>
   </Router>
   );
 }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,13 +4,14 @@ import UsersList from '../components/UsersList';
 const Users = () => {
   const USERS = [
     {
-    id: 'u1',
-    name: 'Jose Pepe',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Georgia_Aquarium_-_Giant_Grouper_edit.jpg/220px-Georgia_Aquarium_-_Giant_Grouper_edit.jpg',
-    places: 3
-  }
+      id: 'u1',
+      name: 'Jose Pepe',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Georgia_Aquarium_-_Giant_Grouper_edit.jpg/220px-Georgia_Aquarium_-_Giant_Grouper_edit.jpg',
+      places: 3
+    }
   ];
-  return <UsersList items={USERS}/>;
-}
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
