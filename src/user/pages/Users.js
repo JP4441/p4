c9b30c7290315e1b2398e8abb7dc49461e6609c1
My Mac <home@Js-MacBook-Pro.local>
2022-04-24 14:46:02 -0400

    P4 update

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>User Works!</h2>
+  const USERS = [
+    {
+    id: 'u1',
+    name: 'Jose Pepe',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Georgia_Aquarium_-_Giant_Grouper_edit.jpg/220px-Georgia_Aquarium_-_Giant_Grouper_edit.jpg',
+    places: 3
+  }
+  ];
+  return <UsersList items={USERS}/>;
 }
 
 export default Users;
